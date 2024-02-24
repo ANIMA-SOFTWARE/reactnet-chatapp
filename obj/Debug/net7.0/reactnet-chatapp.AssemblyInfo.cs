08aa19a5311387f609f4f443e46f7d526048cf6e
scottlewis193 <sl193@pm.me>
2024-02-24 14:25:40 +0000

    init

--- obj/Debug/net7.0/reactnet-chatapp.AssemblyInfo.cs
+++ obj/Debug/net7.0/reactnet-chatapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("reactnet-chatapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25dcba7fe9fafe5680848719f3b1f92e7931280")]
 [assembly: System.Reflection.AssemblyProductAttribute("reactnet-chatapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("reactnet-chatapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
